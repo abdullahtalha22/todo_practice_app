8c29076bf7ebc987b3b84e65f5e8414bdf369623
Abdullah Talha <abdullah.talha22@gmail.com>
2026-04-30 13:29:32 +0500

    added dark mood feature

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -99,4 +99,123 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+}import 'package:flutter/material.dart';
+import '../models/todo_model.dart';
+import '../widgets/todo_tile.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  final TextEditingController controller = TextEditingController();
+
+  List<TodoModel> todos = [];
+
+  bool isDark = false;
+
+  void addTodo() {
+    if (controller.text.isNotEmpty) {
+      setState(() {
+        todos.add(
+          TodoModel(title: controller.text),
+        );
+      });
+
+      controller.clear();
+    }
+  }
+
+  void deleteTodo(int index) {
+    setState(() {
+      todos.removeAt(index);
+    });
+  }
+
+  void toggleComplete(bool? value, int index) {
+    setState(() {
+      todos[index].isCompleted = value ?? false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: isDark
+          ? ThemeData.dark()
+          : ThemeData.light(),
+
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Todo App'),
+
+          actions: [
+
+            Switch(
+              value: isDark,
+              onChanged: (value) {
+                setState(() {
+                  isDark = value;
+                });
+              },
+            ),
+
+          ],
+        ),
+
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+
+          child: Column(
+            children: [
+
+              TextField(
+                controller: controller,
+
+                decoration: const InputDecoration(
+                  hintText: 'Enter Todo',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+
+              const SizedBox(height: 10),
+
+              SizedBox(
+                width: double.infinity,
+
+                child: ElevatedButton(
+                  onPressed: addTodo,
+                  child: const Text('Add Todo'),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              Expanded(
+                child: ListView.builder(
+                  itemCount: todos.length,
+
+                  itemBuilder: (context, index) {
+
+                    return TodoTile(
+                      todo: todos[index],
+                      onDelete: () => deleteTodo(index),
+                      onChanged: (value) =>
+                          toggleComplete(value, index),
+                    );
+
+                  },
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
